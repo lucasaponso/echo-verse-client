fb0b62dab3006c69a98d6f0177fe009b9ee0d55a
Nikhil <nikhil14916@outlook.com>
2025-03-24 21:28:29 +1100

    Created Search component. Need to add search bar. Need to display cards, cards with dummy data, when search button is clicked.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,23 @@
+import { Button, FormControl, Heading } from '@chakra-ui/react';
 import styles from '../styles/search.module.css'
+import { ChangeEvent, useState } from 'react';
 
 export default function Search() {
+
+    const [displayCards, setDisplayCards] = useState(false);
+
+    const onSubmitClick = () => {
+        setDisplayCards(true);
+    }
     return (
-        <div>Test</div>
+        <>
+            <div>Test</div>
+            <Heading as='h2' size='xl'>Looking for something?</Heading>
+            <p>Add search bar here. Consider using another component library like flowbite which has a search bar.</p>
+            <FormControl>
+                <Button colorScheme='blue' onClick={onSubmitClick}>Search</Button>
+            </FormControl>
+            {displayCards===true && <h1>Here are the cards.</h1>}
+        </>
     );
 }
