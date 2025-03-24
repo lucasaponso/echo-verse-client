7c531b9df6d663bdea24e7ba4a6de2dae5ed9c95
Nikhil <nikhil14916@outlook.com>
2025-03-24 15:40:37 +1100

    Mod: Minor change. Edited import statement so that a VSCode error does not appear.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import Navbar from "../components/Navbar.tsx";
+import Navbar from "@/components/Navbar";
 
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
