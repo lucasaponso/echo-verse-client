7cac7ad3069f61d014cd5ad3f3ce241dbd89f2d4
Nikhil <nikhil14916@outlook.com>
2025-03-24 15:16:55 +1100

    Removed git tracking of files in gitignore file by deleting the files, commiting and then adding them back in.

--- .next/server/pages/index.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/index.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_9edc77cb._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__683bf1ba._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9nsWux8Qi9J3MJaMxSN3BGjQbDgV6KlLPxU/Z9qC5e4=\"\n}"
