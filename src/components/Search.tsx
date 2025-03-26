774089690415a0f2a7ebf8c25072c959c3e031c6
Nikhil <nikhil14916@outlook.com>
2025-03-26 17:41:54 +1100

    Added four inputs for search.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, Heading } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Heading, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from '@chakra-ui/react';
 import styles from '../styles/search.module.css'
 import { ChangeEvent, useState } from 'react';
 
@@ -15,8 +15,33 @@ export default function Search() {
         <>
             <Heading paddingTop={10} as='h2' size='xl'>Looking for something?</Heading>
             <p>Add search bar here. Consider using another component library like flowbite which has a search bar.</p>
+
             <FormControl>
-                <Button colorScheme='blue' onClick={onSubmitClick}>Search</Button>
+                <FormLabel>Title</FormLabel>
+                <Input maxWidth='300px'/>
+            </FormControl>
+
+            <FormLabel>Year</FormLabel>
+            <NumberInput maxWidth='300px'>
+            <NumberInputField />
+            <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+            </NumberInputStepper>
+            </NumberInput>
+
+            <FormControl>
+                <FormLabel>Artist</FormLabel>
+                <Input maxWidth='300px'/>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Album</FormLabel>
+                <Input maxWidth='300px'/>
+            </FormControl>
+
+            <FormControl>
+                <Button mt={5} colorScheme='blue' onClick={onSubmitClick}>Search</Button>
             </FormControl>
             {displayCards===true && <Grid></Grid>}
         </>
